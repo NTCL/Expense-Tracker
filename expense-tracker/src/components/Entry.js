544bb79721c421ea1ce9b80ae86b292d62db80cb
Leon <leon990930@gmail.com>
2024-11-03 18:00:06 +0800

    add styles for mobile version (<768px)

--- expense-tracker/src/components/Entry.js
+++ expense-tracker/src/components/Entry.js
@@ -1,24 +1,26 @@
 function Entry({entry, isOdd, isHeader, showExpenseDialog, deleteEntry}) {
     return (
         <div className={`entry ${isHeader ? 'entry-header' : ''} ${isOdd ? 'entry-odd' : ''}`}>
-            <div className={`entry-description ${isHeader ? 'et-p1' : 'et-p3'}`}>
-                {entry.description}
+            <div className='entry-data'>
+                <div className={`entry-data-description ${isHeader ? 'et-p1' : 'et-p3'}`}>
+                    {entry.description}
+                </div>
+                <div className={`entry-data-amount ${isHeader ? 'et-p1' : 'et-p3'}`}>
+                    {`${isHeader ? '' : '$'}${entry.amount}`}
+                </div>
+                <div className={`entry-data-date ${isHeader ? 'et-p1' : 'et-p3'}`}>
+                    {entry.date}
+                </div>
+                <div className={`entry-data-type ${isHeader ? 'et-p1' : 'et-p3'}`}>
+                    {entry.type_id_name === null ? 'Any' : entry.type_id_name}
+                </div>
             </div>
-            <div className={`entry-amount ${isHeader ? 'et-p1' : 'et-p3'}`}>
-                {`${isHeader ? '' : '$'}${entry.amount}`}
-            </div>
-            <div className={`entry-date ${isHeader ? 'et-p1' : 'et-p3'}`}>
-                {entry.date}
-            </div>
-            <div className={`entry-type ${isHeader ? 'et-p1' : 'et-p3'}`}>
-                {entry.type_id_name === null ? 'Any' : entry.type_id_name}
-            </div>
-            {!isHeader ? <div className='entry-buttons'>
+            <div className='entry-buttons' style={{visibility: isHeader ? 'hidden' : 'visible' }}>
                 <button className='et-button et-p4' onClick={() => {
                     showExpenseDialog(entry);
                 }}>Edit</button>
                 <button className='et-button et-p4' onClick={() => deleteEntry(entry)}>Delete</button>
-            </div> : ''}
+            </div>
         </div>
     );
 }
